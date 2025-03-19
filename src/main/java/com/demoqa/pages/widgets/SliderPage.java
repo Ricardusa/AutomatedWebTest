9e222dbfc129ca82e83f5b63886111d76be0aaa9
Ricardusa <rr720p@gmail.com>
2025-03-19 12:49:15 -0500

    Added SliderPage and SliderTest to test slider movement using ActionsUtility. Simulated mouse movements with the Actions class to verify slider functionality.

--- src/main/java/com/demoqa/pages/widgets/SliderPage.java
+++ src/main/java/com/demoqa/pages/widgets/SliderPage.java
@@ -1,8 +1,27 @@
 package com.demoqa.pages.widgets;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+
+import static ultilities.ActionsUtility.dragAndDropBy;
+import static ultilities.GetUtility.getAttribute;
+
 public class SliderPage extends WidgetsPage{
 
+ private By slider = By.xpath("//div[@id='sliderContainer']//input[@type='range']");
+ private By sliderValue = By.id("sliderValue");
 
+ public void moveSlider(int x, int y) {
+     //TODO: Added code within utility class to it simpler and reusable
+     //Actions act = new Actions(driver);
+     //act.dragAndDropBy(find(slider), x, y).perform();
+     //act.perform(); //perform all actions without calling build()
+     dragAndDropBy(find(slider), x, y);
+ }
+
+ public String getSliderValue(){
+     return getAttribute(sliderValue, "value");
+ }
 
 
 }
